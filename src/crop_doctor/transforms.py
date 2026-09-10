3f91ec56907f855325a3416e9d8c6e0fd4a9f29f
malaynilabh <malaynilabh@gmail.com>
2026-09-10 16:12:59 +0530

    Add CNN inference pipeline

---
+++ src/crop_doctor/transforms.py
@@ -0,0 +1,8 @@
+from torchvision import transforms
+
+def create_eval_transform(image_size = (224,224)):
+    return transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
